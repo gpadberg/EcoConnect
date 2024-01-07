adeb972978a4946b9e36ae1eb8fc549c09514dc0
z-codeit <zaaradsouza091@gmail.com>
2024-01-06 19:28:08 -0700

    added log in functionality

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 
-from flask import Flask, render_template, url_for, request, g, redirect
+from flask import Flask, render_template, url_for, request, g, redirect, session
 from database import connect_to_database, getDatabase
-
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.teardown_appcontext
 def close_database(error):
     if hasattr(g, 'eco_db'):
         g.eco_db.close()
+
+def get_current_user():
+    user_result = None
+    if 'user' in session:
+        user = session['user']
+        db = getDatabase()
+        user_cursor = db.execute("select * from users where username = ?", [user])
+        user_result = user_cursor.fetchone()
+    return user_result
 
 @app.route('/')
 def index():
@@ -22,10 +33,20 @@ def login():
 def register():
     if request.method == "POST":
         db = getDatabase()
-        name = request.form['name']
+        name = request.form['username']
         password = request.form['password']
-        db.execute("insert into table users (username, password, points) values (?,?,?)",[name, password, 0])
+
+        user_fetching_cursor = db.execute("select * from users where username = ?")
+        existing_user = user_fetching_cursor.fetchone()
+
+        if existing_user:
+            error = "username already taken, please try again"
+            return render_template("register.html", error=error)
+        
+        db.execute("insert into users (username, password, points) values (?,?,?)",[name, password, 0])
         db.commit()
+        session['name'] = name
+
         return redirect(url_for('index'))
     return render_template("register.html")
 
